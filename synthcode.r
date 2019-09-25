87493b2d1f66b77485e8c65b59eb847f55aeff74
hsantanna <47804685+hsantanna88@users.noreply.github.com>
2019-09-25 15:27:12 -0500

    Update synthcode.r

--- synthcode.r
+++ synthcode.r
@@ -33,18 +33,6 @@ library(SCtools)
 #Uploading the file
 texas <- read.dta13("texas.dta")
 
-texas$state[texas$statefip == 37]
-#>  [1] "North Carolina" "North Carolina" "North Carolina" "North Carolina"
-#>  [5] "North Carolina" "North Carolina" "North Carolina" "North Carolina"
-#>  [9] "North Carolina" "North Carolina" "North Carolina" "North Carolina"
-#> [13] "North Carolina" "North Carolina" "North Carolina" "North Carolina"
-
-unique(texas$statefip)
-#>  [1]  1  2  4  5  6  8  9 10 11 12 13 15 16 17 18 19 20 21 22 23 24 25 26
-#> [24] 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50
-#> [47] 51 53 54 55 56
-
-
 ####Using the Synth Package####
 dataprep.out = dataprep(
   foo = texas,
@@ -758,10 +746,21 @@ placebos = generate.placebos(dataprep.out, synth.out)
 #>  searching for synthetic control unit  
 #> 
 #> Error in svd(c): infinite or missing values in 'x'
-# Breaks during the 34th interaction, which is the 37 state (NC) since some statefip numbers are skipped.
+# Breaks during the 34th interaction, which is the 37th state (NC) since some statefip numbers are skipped.
 
 # To make sure this is not a placebo function related issue, we replaced the 48th state 
 # (the original, which is Texas) with the 37th state (North Carolina) and ran the synth() function again.
+
+#e.g.
+texas$state[texas$statefip == 37]
+#>  [1] "North Carolina" [...]
+
+unique(texas$statefip)
+#>  [1]  1  2  4  5  6  8  9 10 11 12 13 15 16 17 18 19 20 21 22 23 24 25 26
+#> [24] 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 44 45 46 47 48 49 50
+#> [47] 51 53 54 55 56
+
+
 
 dataprep.out = dataprep(
   foo = texas,
@@ -795,8 +794,10 @@ synth.out = synth(data.prep.obj = dataprep.out, method = "BFGS")
 #> Error in svd(c): infinite or missing values in 'x'
 ##As you can see, the function breaks at the infinite or missing value issue.
 
-#We also used the MSCMT package, with no success.
+#%#%#%#%#%#%#%#%#%#%#%#%#%#%#
+#%#%#%#%#%#%#%#%#%#%#%#%#%#%#
 
+#We also used the MSCMT package, with no success.
 ####Using MSCMT####
 texas.list <- listFromLong(texas, unit.variable="statefip", 
                       time.variable="year", unit.names.variable = "state")
